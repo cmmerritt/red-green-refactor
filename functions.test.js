310a10561f4639b4f534ee4f9d7af267cfb8449c
cmmerritt <christiane.merritt@gmail.com>
2021-06-07 17:51:02 -0700

    destructure passes test

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-import { getName } from './functions';
+import getName from './functions';
 
 const spot = { name: 'spot', age: 5, weight: '20 lbs' };
 const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
